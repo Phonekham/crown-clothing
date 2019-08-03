58f0bf224f2ffbe109913769182a30d4831c366c
Phonekham <phone.madridista@gmail.com>
2019-08-03 09:51:12 +0700

    fixed shop error

--- src/components/collections-overview/collections-overview.jsx
+++ src/components/collections-overview/collections-overview.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CollectionPreview from "../collection-preview/collection-preview";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
+import { CollectionsOverviewContainer } from "./collections-overview.styles";
 import { selectCollections } from "../../redux/shop/shop.selector";
 import "./collections-overview.scss";
 
 const CollectionsOverview = ({ collections }) => (
-  <div className="collections-overview">
+  <CollectionsOverviewContainer>
     {collections.map(({ id, ...otherCollectionProps }) => (
       <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
-  </div>
+  </CollectionsOverviewContainer>
 );
 
 const mapStateToiProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToiProps)(CollectionsOverview);
