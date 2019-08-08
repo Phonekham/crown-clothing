6b1bc25f2786a9bea26feb05472ebbf24067961f
Phonekham <phone.madridista@gmail.com>
2019-08-08 11:02:56 +0700

    7. Backend Payment Route

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require("path");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -22,4 +24,19 @@ if (process.env.NODE_ENV === "production") {
 app.listen(port, error => {
   if (error) throw error;
   console.log("server run on port " + port);
+});
+
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.tken.id,
+    amount: req.body.amount,
+    currency: "lak"
+  };
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
